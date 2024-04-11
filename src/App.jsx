92f57ef786457ef9ffc7607291271fb8008d8842
tomi-casabona <tomas.casabona@gmail.com>
2024-04-11 16:18:19 +0200

    Exercise 3 - Layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ const tutorialData = [
     description:
       "Un minim de 30 horas a la setmana. Si no en tens prou, huarás de dedicarli més hores. Al principi sembla imposible peró notaras una millora  rápidament.",
     bgColor: "white",
-    image: "./images/time_management.svg",
+    image: "./public/images/time_managment.svg",
   },
   {
     title: "Programa projectes propis",
     description:
       "Mes val 10 hores treballant en projectes propis que 10 hores mirant tutorials. La motivació i la implementacio en el projecte ajudara a acelerar el teu aprenentatge",
     bgColor: "white",
-    image: "./images/programming.svg",
+    image: "./public/images/programming.svg",
   },
   {
     title: "Procura descansar",
     description:
       "Descansar bé i desconectar son vitals. D'aquesta manera reduiras l'estress y l'ansietat. Milloraras la teva concentració i consolidarás el teu aprenentatge",
     bgColor: "white",
-    image: "./images/meditation.svg",
+    image: "./public/images/meditation.svg",
   },
 ];
 
@@ -36,9 +36,12 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>Sprint-5 React</h1>
-      <Card handleClick={nextStep} count={count} data={tutorialData[count]}></Card>
+    <main className="main">
+      <Card
+        handleClick={nextStep}
+        count={count}
+        data={tutorialData[count]}
+      ></Card>
     </main>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,25 @@
-export const Card = ({ count, data, handleClick }) => {
+export const Card = ({ data, handleClick }) => {
   const handleButtonClick = () => {
     handleClick();
   };
+  console.log(data.image);
 
   return (
-    <section>
-      {data.title && <h2>{data.title}</h2>}
-      {data.description && (
-        <p>
-          <strong>{data.description}</strong>
-        </p>
-      )}
-      <button onClick={handleButtonClick}>Card {count}</button>
-    </section>
+    <>
+      <section className="header">
+        <img className="image" src={data.image} alt="" />
+      </section>
+      <section className="main-container">
+        <div className="content">
+          {data.title && <h2>{data.title}</h2>}
+          {data.description && <p>{data.description}</p>}
+        </div>
+        <img
+          className="button"
+          src="../public/images/right-arrow.png"
+          onClick={handleButtonClick}
+        ></img>
+      </section>
+    </>
   );
 };
